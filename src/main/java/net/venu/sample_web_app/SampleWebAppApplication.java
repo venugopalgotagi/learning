480c290875094095970a35d323698af8b16369da
Venugopal G <gvg9712@gmail.com>
2024-09-14 15:33:29 +0530

    first commit

---
+++ src/main/java/net/venu/sample_web_app/SampleWebAppApplication.java
@@ -0,0 +1,13 @@
+package net.venu.sample_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/venu/sample_web_app/SampleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.venu.sample_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
